e590521dc33962399c60b7ef6d839e4a46307641
emilmadry <emadry@wikia-inc.com>
2018-10-18 16:15:38 +0200

    Network capture restored to config

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,6 @@
 /* global browser */
+const networkCapture = require('./tests/common/networkCapture');
+
 exports.config = {
 	specs: [
 		'./tests/specs/**/*.js'
@@ -59,7 +61,7 @@ exports.config = {
 	waitforTimeout: 10000,
 	connectionRetryTimeout: 90000,
 	connectionRetryCount: 3,
-	services: ['selenium-standalone'],
+	services: ['selenium-standalone', networkCapture],
 	framework: 'mocha',
 	reporters: ['dot', 'allure'],
 
