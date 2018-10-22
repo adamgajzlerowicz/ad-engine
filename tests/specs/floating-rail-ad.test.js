fe55f250fd6cc5e77ae333e3b2b54632ded582b6
wojciechsygier <wsygier@wikia-inc.com>
2018-10-22 15:05:26 +0200

    Enabled networkCapture in tests

--- tests/specs/floating-rail-ad.test.js
+++ tests/specs/floating-rail-ad.test.js
@@ -5,17 +5,27 @@ import adSlots from '../common/adSlots';
 
 const { expect } = require('chai');
 
-let theUrlYouWantToGet;
-
-global.client.on('Network.responseReceived', (params) => {
-	const { url, status } = params.response;
-	if (url.includes('gampad/ads?gdfp')) {
-		theUrlYouWantToGet = url;
-	}
-});
+let fetchedUrl;
+const gatheredUrls = [];
+let i = 0;
 
 describe('Floating rail ads page: floating rail', () => {
-	before(() => {
+	beforeEach(() => {
+		global.clientSelenium.on('Network.responseReceived', (params) => {
+			const { url, status } = params.response;
+			if (url.includes('gampad/ads?gdfp')) {
+				fetchedUrl = url.replace(/.*ads\\?/, '');
+				gatheredUrls[i] = fetchedUrl;
+				i += 1;
+			}
+		});
+
+		afterEach(() => {
+			i = 0;
+			gatheredUrls.length = 0;
+		});
+
+
 		browser.url(floatingRailAd.pageLink);
 		browser.waitForVisible(adSlots.railModule, timeouts.standard);
 	});
@@ -25,6 +35,21 @@ describe('Floating rail ads page: floating rail', () => {
 		expect(browser.element(floatingRailAd.rail).getAttribute(helpers.classProperty))
 			.to
 			.equal(floatingRailAd.attributeRailScrolling, 'Rail did not scroll');
+		// console.log(gatheredUrls);
+		expect(gatheredUrls[1]).to.include('cookie');
+		expect(browser.isVisibleWithinViewport(floatingRailAd.rail, 'Rail not in viewport'))
+			.to
+			.be
+			.true;
+	});
+
+	it('Check if rail scrolls with the content', () => {
+		helpers.slowScroll(500);
+		expect(browser.element(floatingRailAd.rail).getAttribute(helpers.classProperty))
+			.to
+			.equal(floatingRailAd.attributeRailScrolling, 'Rail did not scroll');
+		// console.log(gatheredUrls);
+		expect(gatheredUrls[1]).to.include('cookie');
 		expect(browser.isVisibleWithinViewport(floatingRailAd.rail, 'Rail not in viewport'))
 			.to
 			.be
